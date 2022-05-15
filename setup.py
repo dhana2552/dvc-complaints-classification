73293a41cfdbfcb3d0cfb739e3544f010008ed3f
aimllearner <dhana2552@gmail.com>
2022-05-15 21:30:07 +0530

    Updated repo name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 ## edit below variables as per your requirements -
-REPO_NAME = "census-project"
+REPO_NAME = "dvc-complaints-classification"
 AUTHOR_USER_NAME = "dhana2552"
 SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = []
